019582b7b7c86c70a523485a72a86362f83360c8
Ssslakter <chauninslava2003@gmail.com>
2022-04-20 00:04:25 +0700

    stuff

--- app/src/main/java/com/example/bruhdroid/CodingActivity.kt
+++ app/src/main/java/com/example/bruhdroid/CodingActivity.kt
@@ -1,11 +1,36 @@
 package com.example.bruhdroid
 
+import android.content.ClipData
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.DragEvent
+import android.view.View
 
 class CodingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coding)
+
+    }
+    val dragListener=View.OnDragListener{view,event->
+        when(event.action){
+            DragEvent.ACTION_DRAG_STARTED->{
+                println("hello")
+                true
+            }
+            else->true
+        }
+    }
+
+    private fun dragBlock() {
+        val dragBlock: View = findViewById(R.id.blockView)
+        dragBlock.setOnLongClickListener {
+            println("lel")
+            val dragShadowBuilder=View.DragShadowBuilder(it)
+            it.startDragAndDrop(null,dragShadowBuilder,it,0)
+            it.visibility=View.INVISIBLE
+            true
+        }
     }
 }
