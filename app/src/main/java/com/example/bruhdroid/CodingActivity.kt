d84350783a4077c989c7fff6865668c7e72b8c9c
Ssslakter <chauninslava2003@gmail.com>
2022-04-21 01:26:07 +0700

    micro win

--- app/src/main/java/com/example/bruhdroid/CodingActivity.kt
+++ app/src/main/java/com/example/bruhdroid/CodingActivity.kt
@@ -3,16 +3,21 @@ package com.example.bruhdroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import com.example.bruhdroid.model.Instruction
 import com.example.bruhdroid.model.blocks.Block
 import com.example.bruhdroid.model.Interpreter
+import com.example.bruhdroid.model.blocks.Init
+import com.example.bruhdroid.model.blocks.RawInput
+import kotlin.math.exp
 
 class CodingActivity : AppCompatActivity() {
 
-    var blocks: MutableList<Block> = mutableListOf()
+    var viewBlocks: MutableList<View> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coding)
@@ -22,18 +27,31 @@ class CodingActivity : AppCompatActivity() {
             buildBlock()
         }
 
+        val launch: Button = findViewById(R.id.launchButton)
+        launch.setOnClickListener {
+            Launch()
+        }
     }
 
     private fun buildBlock() {
         val layout: LinearLayout = findViewById(R.id.container)
         val view = layoutInflater.inflate(R.layout.block, null)
-        println(view.findViewById<EditText>(R.id.variable).text)
         layout.addView(view)
-        //blocks.add(Block.(Instruction.SET,))
+        viewBlocks.add(view)
     }
 
     private fun Launch() {
-        //val interpreter: Interpreter = Interpreter()
+        var blocks: MutableList<Block> = mutableListOf()
+        for (view in viewBlocks) {
+            val variable = view.findViewById<EditText>(R.id.variable).getText().toString()
+            val expression = view.findViewById<EditText>(R.id.expression).getText().toString()
+            blocks.add(Init(variable, RawInput(expression)))
+        }
+        val interpreter: Interpreter = Interpreter(blocks)
+        interpreter.run()
+        println(interpreter.memory.pop("a").value)
+        println(interpreter.memory.pop("b").value)
+
     }
 
 }
