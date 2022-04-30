e6b425def35d06406479282583694c7bda61557a
Cortuzz <oleg.cortuzz1@gmail.com>
2022-05-01 02:47:58 +0700

    Better connections

--- app/src/main/java/com/example/bruhdroid/CodingActivity.kt
+++ app/src/main/java/com/example/bruhdroid/CodingActivity.kt
@@ -28,7 +28,7 @@ class CodingActivity : AppCompatActivity(), Observer {
     private var currentDragIndex = 0
     private var viewToBlock = mutableMapOf<View, Block>()
     private var viewList = mutableListOf<View>()
-    private var connectorsMap = mutableMapOf<View, Int>()
+    private var connectorsMap = mutableMapOf<View, View>()
     private var prevBlock: View? = null
 
     private lateinit var binding : ActivityCodingBinding
@@ -151,10 +151,11 @@ class CodingActivity : AppCompatActivity(), Observer {
         }
     }
 
-    private fun replaceUntilEnd(indexLast: Int, indexNew: Int) {
-        val endInstructions = listOf(Instruction.END, Instruction.END_WHILE)
+    private fun replaceUntilEnd(indexLast: Int, indexNew: Int): List<Int> {
+        val endInstructions = listOf(Instruction.END, Instruction.END_WHILE) //todo: elif / else check
         val startInstructions = listOf(Instruction.IF, Instruction.WHILE)
         val tempViews = mutableListOf<View>()
+        var height = 0
         var count = 0
 
         do {
@@ -165,12 +166,16 @@ class CodingActivity : AppCompatActivity(), Observer {
                 else -> {}
             }
 
+            height += view.height + 10
             tempViews.add(view)
         } while(count > 0)
 
+        val size: Int = tempViews.size
         while (tempViews.size > 0) {
             viewList.add(indexNew, tempViews.removeLast())
         }
+
+        return listOf(height, size)
     }
 
     private fun clearConstraints(set: ConstraintSet, view: View) {
@@ -181,7 +186,7 @@ class CodingActivity : AppCompatActivity(), Observer {
         set.clear(id, ConstraintSet.LEFT)
         set.clear(id, ConstraintSet.RIGHT)
 
-        val connector = connectorsMap[view]
+        val connector = connectorsMap[view]?.id
         if (connector != null) {
             set.clear(connector, ConstraintSet.TOP)
             set.clear(connector, ConstraintSet.BOTTOM)
@@ -190,43 +195,32 @@ class CodingActivity : AppCompatActivity(), Observer {
         }
     }
 
-    private fun reBuildBlocks(index: Int, drag: View, untilEnd: Boolean = false) {
+    private fun getRatio(ind: Int, y1: Float, y2: Float, h: Int, new: Int, old: Int, c: Int, cycleLength: Int): Float? {
+        val prevSize = y1 - y2
+        val cycleStart = ind - cycleLength - 1
+        val decrease = (new + c > ind || new <= cycleStart) && cycleStart < old + c && old + c <= ind
+        val increase = (old + c > ind || old <= cycleStart) && cycleStart < new && new + c <= ind // todo: cycleStart < new ? c ?
+
+        if (decrease && increase) {
+            return null
+        }
+
+        if (decrease) {
+            return prevSize - h
+        }
+        if (increase) {
+            return prevSize + h
+        }
+        return null
+    }
+
+    private fun buildConstraints(set: ConstraintSet, height: Int, newIndex: Int, lastIndex: Int, count: Int) {
         val nestViews = mutableListOf<View>()
         val nestCount = mutableListOf<Int>()
-        val set = ConstraintSet()
-        set.clone(binding.container)
-        for  (i in 0 until viewList.size) {
-            clearConstraints(set, viewList[i])
-        }
-
-        if (untilEnd) {
-            replaceUntilEnd(viewList.indexOf(drag), index)
-        } else {
-            viewList.remove(drag)
-            if (index > viewList.lastIndex) {
-                viewList.add(drag)
-            } else {
-                viewList.add(index, drag)
-            }
-
-        }
 
         for (i in 1 until viewList.size) {
             val view = viewList[i]
             val prevView = viewList[i - 1]
-            if (viewToBlock[prevView]!!.instruction == Instruction.WHILE &&
-                    viewToBlock[view]!!.instruction == Instruction.END_WHILE) {
-
-                val nest = prevView.id
-                val connector = connectorsMap[view]!!
-                set.connect(view.id, ConstraintSet.TOP, prevView.id, ConstraintSet.BOTTOM, 50)
-                set.connect(view.id, ConstraintSet.LEFT, prevView.id, ConstraintSet.LEFT, 0)
-
-                set.connect(connector, ConstraintSet.TOP, nest, ConstraintSet.BOTTOM, 0)
-                set.connect(connector, ConstraintSet.BOTTOM, view.id, ConstraintSet.TOP, 0)
-                set.connect(connector, ConstraintSet.LEFT, nest, ConstraintSet.LEFT, 0)
-                continue
-            }
 
             if (viewToBlock[prevView]!!.instruction == Instruction.WHILE) {
                 nestViews.add(prevView)
@@ -234,16 +228,19 @@ class CodingActivity : AppCompatActivity(), Observer {
             }
 
             if (viewToBlock[view]!!.instruction == Instruction.END_WHILE) {
-                val count = nestCount.removeLast()
-                val nest = nestViews.removeLast().id
+                val nest = nestViews.removeLast()
+                val nestId = nest.id
                 val connector = connectorsMap[view]!!
-                set.setScaleY(connector, count * 1f)
+                val connectorId = connector.id
 
-                set.connect(connector, ConstraintSet.TOP, nest, ConstraintSet.BOTTOM, 0)
-                set.connect(connector, ConstraintSet.BOTTOM, view.id, ConstraintSet.TOP, 0)
-                //set.connect(view.id, ConstraintSet.LEFT, nest, ConstraintSet.LEFT, 150)
+                val ratio = getRatio(i, view.y, nest.y, height, newIndex, lastIndex, count, nestCount.removeLast())?.div(connector.height)
+                if (ratio != null) {
+                    set.setScaleY(connectorId, ratio)
+                }
 
-                set.connect(connector, ConstraintSet.LEFT, nest, ConstraintSet.LEFT, 0)
+                set.connect(connectorId, ConstraintSet.TOP, nestId, ConstraintSet.BOTTOM, 0)
+                set.connect(connectorId, ConstraintSet.BOTTOM, view.id, ConstraintSet.TOP, 0)
+                set.connect(connectorId, ConstraintSet.LEFT, nestId, ConstraintSet.LEFT, 0)
             }
 
             if (nestViews.isNotEmpty()) {
@@ -253,6 +250,37 @@ class CodingActivity : AppCompatActivity(), Observer {
 
             set.connect(view.id, ConstraintSet.TOP, prevView.id, ConstraintSet.BOTTOM, 10)
         }
+    }
+
+    private fun reBuildBlocks(index: Int, drag: View, untilEnd: Boolean = false) {
+        val set = ConstraintSet()
+        val lastIndex = viewList.indexOf(drag)
+        val blockHeight: Int
+        val count: Int
+
+        set.clone(binding.container)
+        for  (i in 0 until viewList.size) {
+            clearConstraints(set, viewList[i])
+        }
+
+        if (untilEnd) {
+            val data = replaceUntilEnd(viewList.indexOf(drag), index)
+            blockHeight = data[0]
+            count = data[1]
+        } else {
+            blockHeight = drag.height
+            count = 1
+
+            viewList.remove(drag)
+            if (index > viewList.lastIndex) {
+                viewList.add(drag)
+            } else {
+                viewList.add(index, drag)
+            }
+
+        }
+
+        buildConstraints(set, blockHeight, index, lastIndex, count)
         prevBlock = viewList.last()
         set.applyTo(binding.container)
     }
@@ -321,8 +349,8 @@ class CodingActivity : AppCompatActivity(), Observer {
         prevBlock = view
 
         if (endBlock != null && connector != null) {
-            connectorsMap[endBlock] = connector.id
-            set.connect(endBlock.id, ConstraintSet.TOP, view.id, ConstraintSet.BOTTOM, 50)
+            connectorsMap[endBlock] = connector
+            set.connect(endBlock.id, ConstraintSet.TOP, view.id, ConstraintSet.BOTTOM, 10)
 
             set.connect(connector.id, ConstraintSet.TOP, view.id, ConstraintSet.BOTTOM, -100)
             set.connect(connector.id, ConstraintSet.BOTTOM, endBlock.id, ConstraintSet.TOP, -100)
